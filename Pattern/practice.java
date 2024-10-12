851ad945835de6ad245637e73af857d4c97901d8
Kaushal-Dalal <kaushaldalal879@gmail.com>
2024-10-12 11:19:32 +0530

    12-10-2024

---
+++ Pattern/practice.java
@@ -0,0 +1,25 @@
+package pattern;
+public class practice {
+   public static void main(String[] args) {
+    int[] arr = {1,2,3,9,4,2,4};
+    int n =arr.length;
+    int[] ans = new int[n];
+    ans[n-1] = -1;
+    int mx = 0;
+    for(int i=0;i<arr.length-1;i++){
+        
+        for(int j=i+1;j<arr.length;j++){
+           if(mx<arr[j]){
+            int temp = mx;
+            mx = arr[j];
+            arr[j] = temp;
+           }
+           
+        }
+        ans[i] = mx;
+    }
+    for(int ele : ans){
+        System.out.print(ele+" ");
+    }
+   }   
+}
